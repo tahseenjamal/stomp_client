6c797f9ade17756c63502122c028ace936c691db
tahseen.jamal <tahseen.jamal@gmail.com>
2023-12-14 00:36:52 +0530

    modified for automatic reconnect and reattempt submission so as to not lose the message

--- producer/producer.go
+++ producer/producer.go
@@ -8,38 +8,89 @@ import (
 	"github.com/go-stomp/stomp"
 )
 
-func main() {
-	// ActiveMQ broker details
-	broker := "localhost"
-	port := "61613"
-	username := ""
-	password := ""
+var conn *stomp.Conn
+var err error
 
-	// Create connection with Reconnect option and HeartBeatGracePeriodMultiplier
-	conn, err := stomp.Dial("tcp", broker+":"+port,
-		stomp.ConnOpt.Login(username, password),
-		stomp.ConnOpt.AcceptVersion(stomp.V12),
-		stomp.ConnOpt.HeartBeat(5*time.Second, 5*time.Second), // Set the initial heartbeats
-		stomp.ConnOpt.HeartBeatGracePeriodMultiplier(5),       // Set HeartBeatGracePeriodMultiplier
-	)
-	if err != nil {
-		log.Fatal(err)
+type packet struct {
+	queue   string
+	message string
+}
+
+var brokerQueue chan packet
+
+func getSession() *stomp.Conn {
+
+	for {
+
+		fmt.Println("Attempting connection")
+		// ActiveMQ broker details
+		broker := "localhost"
+		port := "61613"
+		username := ""
+		password := ""
+		// Create connection with Reconnect option and HeartBeatGracePeriodMultiplier
+		conn_local, err := stomp.Dial("tcp", broker+":"+port,
+			stomp.ConnOpt.Login(username, password),
+			stomp.ConnOpt.AcceptVersion(stomp.V12),
+			stomp.ConnOpt.HeartBeat(5*time.Second, 5*time.Second), // Set the initial heartbeats
+			stomp.ConnOpt.HeartBeatGracePeriodMultiplier(5),       // Set HeartBeatGracePeriodMultiplier
+		)
+
+		if err != nil {
+			log.Println(err)
+			time.Sleep(5 * time.Second)
+		} else {
+
+			fmt.Println("connected")
+			return conn_local
+		}
+
 	}
-	defer conn.Disconnect()
+
+}
+
+func sendMessage() {
+
+	for {
+
+		data := <-brokerQueue
+		queue := data.queue
+		message := data.message
+
+		// Will break out of this loop on successful connection and
+		// message delivery. So messages are not lost
+		for {
+
+			err = conn.Send(queue, "text/plain", []byte(message))
+			if err != nil {
+				log.Printf("Error sending message to %s: %s", queue, err)
+				conn = getSession()
+				fmt.Println("Inside send message connected")
+			} else {
+				fmt.Printf("Message sent to %s: %s\n", queue, message)
+				break
+			}
+		}
+	}
+}
+
+func init() {
+
+	brokerQueue = make(chan packet)
+	conn = getSession()
+	go sendMessage()
+}
+
+func main() {
 
 	// Define your queues
-	queue1 := "/queue/queue1"
+	queue := "/queue/queue1"
 
-	// Message content
-	message := "Hello, ActiveMQ!"
+	for i := 1; i <= 20; i++ {
 
-	// Send message to both queues
-	err = conn.Send(queue1, "text/plain", []byte(message))
-	if err != nil {
-		log.Printf("Error sending message to %s: %s", queue1, err)
-	} else {
-		fmt.Printf("Message sent to %s: %s\n", queue1, message)
+		// Message content
+		message := fmt.Sprintf("Message number %d", i)
+		brokerQueue <- packet{queue, message}
+		time.Sleep(time.Second * 2)
 	}
-
-	time.Sleep(time.Second * 2)
 }
